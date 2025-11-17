6dab48b95bec40140688fde99a0b5f36b79f400b
priteshrao3 <priteshrao3@gmail.com>
2025-11-18 00:19:57 +0530

    app updated

--- wikinstape/services/admin.py
+++ wikinstape/services/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from .models import RoleServicePermission, UserServicePermission
-
-@admin.register(RoleServicePermission)
-class RoleServicePermissionAdmin(admin.ModelAdmin):
-    list_display = ['role', 'service_category', 'service_subcategory', 'is_active', 'can_view', 'can_use']
-    list_filter = ['role', 'is_active']
-    search_fields = ['role', 'service_category__name', 'service_subcategory__name']
-
-@admin.register(UserServicePermission)
-class UserServicePermissionAdmin(admin.ModelAdmin):
-    list_display = ['user', 'service_category', 'service_subcategory', 'is_active', 'can_view', 'can_use']
-    list_filter = ['user__role', 'is_active']
-    search_fields = ['user__username', 'service_category__name', 'service_subcategory__name']
