8380f7e0d52de5c9fb934dc204934848ce460cac
pritesh <pritesh@pritesh.pritesh>
2025-12-08 00:00:52 +0530

    app updated

--- wikinstape/vendorpayment/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-# from .views import VendorPaymentViewSet
-
-# vendor_payment = VendorPaymentViewSet.as_view
-
-# urlpatterns = [
-#     path("pay/", vendor_payment({"post": "pay"})),
-# ]
