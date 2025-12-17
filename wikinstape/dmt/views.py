901a7ed30bb8bd64af46dcc211f6e9b9e49473b5
priteshrao3 <priteshrao3@gmail.com>
2025-12-17 16:54:01 +0530

    app updated

--- wikinstape/dmt/views.py
+++ wikinstape/dmt/views.py
@@ -285,10 +285,9 @@ class DMTTransactionViewSet(viewsets.ViewSet):
         # ---------- STEP 5: RECIPIENT ----------
         try:
             recipient = DMTRecipient.objects.get(
-                bank_recipient_id=data['recipient_id'],
+                eko_recipient_id=data['recipient_id'],
                 user=user
             )
-
         except DMTRecipient.DoesNotExist:
             return Response({
                 "status": 1,
